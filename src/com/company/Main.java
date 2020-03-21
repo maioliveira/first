579e68b146497d65b6b3cfb0c90f7b349ec99fb9
Maiza Oliveira <maiza.oliveira54@gnail.com>
2020-03-20 23:15:26 -0300

    FOR

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,58 +11,28 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String[] meuArray = new String[10];
-int arrayLength = meuArray.length;
-int contador = 0;
+
+        int arrayLength = meuArray.length;
+        int contador = 0;
         while (contador < arrayLength) {
-            System.out.println( "Write a name" );
+            System.out.println("Write a name");
             meuArray[contador] = new String(reader.readLine());
-            System.out.println( "You put " + meuArray[contador]);
+            System.out.println("You put " + meuArray[contador]);
 
-            contador = contador + 1;
+            contador++;
         }
-        //String name1, name2, name3, name4, name5, name6, name7, name8, name9, name10;
-/*
-        System.out.println( "Write a name" );
-                meuArray[0] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[0]);
 
-        System.out.println( "Write another name" );
-                meuArray[1] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[1]);
+        //int contador2 = 0;
+        //while (contador2 < arrayLength) {
+        for (int contador2 = 0; contador2 < arrayLength; contador2++) {
+            System.out.print(meuArray[contador2]);
 
-        System.out.println( "Write another name" );
-                meuArray[2] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[2]);
+          //contador2++;
 
-        System.out.println( "Write another name" );
-                meuArray[3] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[3]);
+            if (! (contador2 >= arrayLength)) {
+                System.out.print(" ❤ ");
 
-        System.out.println( "Write another name" );
-                meuArray[4] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[4]);
-
-        System.out.println( "Write another name" );
-                meuArray[5] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[5]);
-
-        System.out.println( "Write another name" );
-                meuArray[6] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[6]);
-
-        System.out.println( "Write another name" );
-                meuArray[7] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[7]);
-
-        System.out.println( "Write another name" );
-                meuArray[8] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[8]);
-
-        System.out.println( "Write one last name" );
-                meuArray[9] = reader.readLine();
-                    System.out.println( "You wrote " + meuArray[9]);
-*/
-        System.out.println(meuArray[0] + " ❤ " + meuArray[1] + " ❤ " + meuArray[2] + " ❤ " + meuArray[3] + " ❤ " + meuArray[4] + " ❤ " + meuArray[5] + " ❤ " + meuArray[6] + " ❤ " + meuArray[7] + " ❤ " + meuArray[8] + " ❤ " + meuArray[9]);
-
+            }
+        }
     }
 }
